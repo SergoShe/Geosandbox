d422534d5c44febe37efde52ec4668529bdab960
SergoShe <snow5373@gmail.com>
2022-10-12 01:20:47 +0700

    Some changes

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         try {
-            Shape shape = ShapeBuilder.build();
+            ShapeBuilder shapeBuilder = new ShapeBuilder();
+            Shape shape = shapeBuilder.build();
             System.out.println("Area: " + shape.getArea());
             System.out.println("Perimeter: " + shape.getPerimeter());
         } catch (IOException e) {
--- src/ShapeBuilder.java
+++ src/ShapeBuilder.java
@@ -4,12 +4,12 @@ import java.io.InputStreamReader;
 
 public class ShapeBuilder {
 
-    public static Shape build() throws IOException {
+    public Shape build() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter figure number:");
         System.out.println("1.Rectangle\n2.Triangle\n3.Circle");
-        Figure figure = Figure.valueOf(Integer.parseInt(reader.readLine()));
-        return switch (figure) {
+        InputShapeType shapeType = InputShapeType.valueOf(Integer.parseInt(reader.readLine()));
+        return switch (shapeType) {
             case RECTANGLE -> createRectangle();
 
             case TRIANGLE -> createTriangle();
@@ -18,7 +18,7 @@ public class ShapeBuilder {
         };
     }
 
-    private static Shape createRectangle() throws IOException {
+    private Shape createRectangle() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Enter side A: ");
         double sideA = Double.parseDouble(reader.readLine());
@@ -27,7 +27,7 @@ public class ShapeBuilder {
         return new Rectangle(sideA, sideB);
     }
 
-    private static Shape createTriangle() throws IOException {
+    private Shape createTriangle() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Enter side A: ");
         double sideA = Double.parseDouble(reader.readLine());
@@ -38,7 +38,7 @@ public class ShapeBuilder {
         return new Triangle(sideA, sideB, sideC);
     }
 
-    private static Shape createCircle() throws IOException {
+    private Shape createCircle() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Enter radius: ");
         double radius = Double.parseDouble(reader.readLine());
